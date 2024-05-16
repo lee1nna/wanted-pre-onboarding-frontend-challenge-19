24fc3043f640ac79c2922f14ec401eabe7d8a7fa
lee1nna <dlgkssk0209@gmail.com>
2024-05-16 12:55:59 +0900

    Chore: add eslint, prettier config

--- src/components/ListItem.tsx
+++ src/components/ListItem.tsx
@@ -1,26 +1,24 @@
-import { List } from "../model/List"
-import { useAppDispatch, useAppSelector } from "../store"
-import { deleteList } from "../store/ListSlice"
-import styles from './ListItem.module.css'
+import { List } from '../model/List';
+import { useAppDispatch, useAppSelector } from '../store';
+import { deleteList } from '../store/ListSlice';
+import styles from './ListItem.module.css';
 
-const ListItem = () => {
-    const {lists} = useAppSelector(state => state.list)
-    const dispatch = useAppDispatch()
+function ListItem() {
+  const { lists } = useAppSelector((state) => state.list);
+  const dispatch = useAppDispatch();
 
-    return (
-        <div className={styles['item-wrapper']}>
-        {
-            lists.map((list:List, idx) => {
-                return (
-                <div key={`list-${list.id}`} className={styles['item']}>
-                    <div>{list.title}</div>
-                    <button onClick={() => {dispatch(deleteList(idx))}}>Delete</button>
-                </div>
-                )
-            })
+  return (
+    <div className={styles['item-wrapper']}>
+      {
+            lists.map((list:List, idx) => (
+              <div key={`list-${list.id}`} className={styles.item}>
+                <div>{list.title}</div>
+                <button onClick={() => { dispatch(deleteList(idx)); }}>Delete</button>
+              </div>
+            ))
         }
-        </div>
-    )
+    </div>
+  );
 }
 
-export default ListItem
+export default ListItem;
